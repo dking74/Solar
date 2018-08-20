64c4de821196eb96cb0c79a8ef1feee66c5e295b
Devon King <djkdkc@mail.missouri.edu>
2018-08-20 09:33:46 -0500

    Updated test

--- Map_Solar/Test_Delete.py
+++ Map_Solar/Test_Delete.py
@@ -1,2 +1,27 @@
-# testing
-# testing
+from orionsdk import SwisClient
+import requests
+
+while not valid:
+
+    username = input ( "Username: " )
+    password = input ( "Password: " )
+    print            (     "\n"     )
+
+    try: 
+        solarwinds = SwisClient ( "solarwinds.ameren.com" , username , password )
+        solarwinds.query        (     "SELECT AccountID FROM Orion.Accounts"    )
+        valid = True
+
+    except requests.exceptions.HTTPError:
+        valid = False
+
+results = solarwinds.query (    """
+                                SELECT
+                                    m.MACAddressInfo.MACAddress,
+                                    m.Ports.Name
+                                FROM
+                                    Orion.Nodes m
+                                """
+                            )
+
+print ( results )
