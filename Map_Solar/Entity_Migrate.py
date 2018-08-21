ee4ce6357b9de4c3b14c0e045e8886e276316ae0
Devon King <djkdkc@mail.missouri.edu>
2018-08-21 09:17:15 -0500

    fixed: bug

--- Map_Solar/Entity_Migrate.py
+++ Map_Solar/Entity_Migrate.py
@@ -421,16 +421,6 @@ class IntelligridMig ( ):
         # get container info to see if a group is present
         container = self.getGroupID ( group_name )
 
-        query = self._solarwinds.query  (   """
-                                            SELECT
-                                                InstanceType
-                                            FROM
-                                                Orion.Nodes
-                                            """
-                                        )
-
-        print ( query )
-
         # there are no results, add the group
         if container == "None":
             try:
