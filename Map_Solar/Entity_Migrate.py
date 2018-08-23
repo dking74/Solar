8f47c183a03151a00ba588f85fa0d0fcf4e2ede3
Devon King <djkdkc@mail.missouri.edu>
2018-08-23 16:05:16 -0500

    Update: Cleanup

--- Map_Solar/Entity_Migrate.py
+++ Map_Solar/Entity_Migrate.py
@@ -758,12 +758,11 @@ class IntelligridMig  ( ):
                                                 """.format ( node_name )
                                              )
 
-        counter = 0
+        print ( existingGroups )
+        print ( node_group [ 'results' ] )
+        
         for group_holder in node_group [ 'results' ]:
             #print ( group_holder [ 'Member' ] , group_holder [ 'Name' ] )
-            print ( group_holder [ 'Name' ] )
-            print ( existingGroups [ counter ] )
-            counter += 1
             if group_holder [ 'Name' ] in existingGroups:
                 print ( "The group already exists" )
 
