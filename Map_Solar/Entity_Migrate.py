8dfa639ce635a70e69de17208f00b2e989880716
Devon King <djkdkc@mail.missouri.edu>
2018-08-24 10:52:15 -0500

    Update: Cleanup

--- Map_Solar/Entity_Migrate.py
+++ Map_Solar/Entity_Migrate.py
@@ -785,9 +785,7 @@ class IntelligridMig  ( ):
 
         # if there is container info available, see if there is one matching the existing groups
         if len ( result_list ) > 0:
-            return  [  [ True, group_holder [ 'Name' ], group_holder [ 'ID' ] ] \
-                        for group_holder in result_list if group_holder [ 'Name' ] in existingGroups
-                    ]
+            return  [  [ True, group_holder [ 'Name' ], group_holder [ 'ID' ] ] for group_holder in result_list if group_holder [ 'Name' ] in existingGroups ]
 
         # return that the group did not exist
         return [ False, "None", 0 ]
