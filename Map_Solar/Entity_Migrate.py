297eaa9b60fee3d95a7b4931fda9cf18f1b97d8d
Devon King <djkdkc@mail.missouri.edu>
2018-09-05 09:20:59 -0500

    Update: Cleanup

--- Map_Solar/Entity_Migrate.py
+++ Map_Solar/Entity_Migrate.py
@@ -981,15 +981,15 @@ class IntelligridMig  ( ):
         # test function for querying data
         result = self._solarwinds.query (   """
                                             SELECT
-                                                Tolocal ( r.DateTime )         as EventDate,
-                                                Year    Tolocal ( r.DateTime ) as Year,
-                                                Month   Tolocal ( r.DateTime ) as Month,
-                                                Day     Tolocal ( r.DateTime ) as Day,
-                                                Hour    Tolocal ( r.DateTime ) as Hour,
-                                                Minute  Tolocal ( r.DateTime ) as Minute,
-                                                Second  Tolocal ( r.DateTime ) as Second,
-                                                WeekDay Tolocal ( r.DateTime ) as WeekDay,
-                                                r.Availability                 as Available,
+                                                Tolocal ( r.DateTime )             as EventDate,
+                                                Year    ( Tolocal ( r.DateTime ) ) as Year,
+                                                Month   ( Tolocal ( r.DateTime ) ) as Month,
+                                                Day     ( Tolocal ( r.DateTime ) ) as Day,
+                                                Hour    ( Tolocal ( r.DateTime ) ) as Hour,
+                                                Minute  ( Tolocal ( r.DateTime ) ) as Minute,
+                                                Second  ( Tolocal ( r.DateTime ) ) as Second,
+                                                WeekDay ( Tolocal ( r.DateTime ) ) as WeekDay,
+                                                r.Availability                     as Available,
                                                 GetUTCDate( )          as DateUTC,
                                                 GetDate ( )            as DateReg
                                             FROM
