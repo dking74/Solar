e138a5f06802051f1a3e4a3ca65136b7c2304410
Devon King <djkdkc@mail.missouri.edu>
2018-09-07 16:34:16 -0500

    Update: Functionality change

--- Solar_Scripts/PortInfo/portinfo.py
+++ Solar_Scripts/PortInfo/portinfo.py
@@ -6,7 +6,7 @@ class ExcelSheet ( ):
 
 	'''
 	Class name    : ExcelSheet
-	
+
 	Class Purpose : To handle reading and writing to Excel spreadsheet
 	'''
 
@@ -14,12 +14,12 @@ class ExcelSheet ( ):
 
 		'''
 		Method name: __init__
-		
+
 		Method Purpose: To initialize an excel instance
-		
+
 		Parameters:
 			- workbook (string): The name of the workbook
-		
+
 		Returns: None
 		'''
 
@@ -33,15 +33,15 @@ class ExcelSheet ( ):
 
 		'''
 		Method name: readFromWorkbook
-		
+
 		Method Purpose: To read from the inputted workbook
-		
+
 		Parameters:
 			- numRows (integer): The number of rows to read
 			- numColumns (integer): The number of columns to read
 			- startRow (integer): The row to start reading from
 			- sheet (unknown): The worksheet to read from
-		
+
 		Returns: A list of all data
 		'''
 
@@ -49,29 +49,29 @@ class ExcelSheet ( ):
 		for sheet_row in range ( *self.__getReadRange ( sheet.max_row , numRows , startRow ) ):
 			rowData = self.readRowFromWorkbook ( sheet_row , numColumns , sheet )
 			spreadsheet_data.append ( rowData )
-		return spreadsheet_data	
+		return spreadsheet_data
 
 	def readRowFromWorkbook     ( self , rowNum , numColumns , sheet ):
 
 		'''
 		Method name: readRowFromWorkbook
-		
+
 		Method Purpose: To read an individual row from Workbook
-		
-		Parameters: 
+
+		Parameters:
 			- rowNum (integer): The numbered row to read
 			- numColumns (integer): The number of columns to read from row
 			- sheet (unknown): The sheet that we are reading from
-		
-		Returns: A list of the data in the row
-		'''	
 
-		rowData = 	[ 
-						sheet.cell  ( 
+		Returns: A list of the data in the row
+		'''
+
+		rowData = 	[
+						sheet.cell  (
 							row=rowNum if rowNum < sheet.max_row + 1 else sheet.max_row, \
-							column=column 
+							column=column
 						).value for column in range ( \
-							*self.__getReadRange ( sheet.max_column , numColumns ) 
+							*self.__getReadRange ( sheet.max_column , numColumns )
 						)
 					]
 		return rowData
@@ -80,11 +80,11 @@ class ExcelSheet ( ):
 
 		'''
 		Method name: writeToWorkbook
-		
+
 		Method Purpose: To write to the inputted workbook
-		
+
 		Parameters: None
-		
+
 		Returns: None
 		'''
 
@@ -92,12 +92,12 @@ class ExcelSheet ( ):
 
 		'''
 		Method name: removeSheetFromWorkbook
-		
+
 		Method Purpose: To remove an inputted sheet from the inputted workbook
-		
+
 		Parameters:
 			- sheetName (string): The name of the sheet to remove
-		
+
 		Returns: None
 		'''
 
@@ -111,21 +111,21 @@ class ExcelSheet ( ):
 
 		'''
 		Method name: __findReadRange
-		
+
 		Method Purpose: To find the range being read from Excel Worksheet;
 						either the row or column range
-		
+
 		Parameters:
 			- maxCell (integer): The max number of cells that can be read
 			- startCell (integer): The starting cell (row or column)
 			- inputCell (integer): The user inputted range to be read
-		
+
 		Returns: A tuple containing the read range
 		'''
 
-		cellRange = ( 
+		cellRange = (
 				startCell    if startCell < inputCell and startCell > 0
-							else 1 , 
+							else 1 ,
 				inputCell + 1 if inputCell  < maxCell + 1 and inputCell < maxCell + 1 \
 							else maxCell + 1
 			)
@@ -135,11 +135,11 @@ class ExcelSheet ( ):
 
 		'''
 		Method name: __getWorkbookActiveSheet
-		
+
 		Method Purpose: Get the Workbooks active sheet
-		
+
 		Parameters: None
-		
+
 		Returns: The active sheet of the workbook
 		'''
 
@@ -149,12 +149,12 @@ class ExcelSheet ( ):
 
 		'''
 		Method name: _openWorkbook
-		
+
 		Method Purpose: To open the workbook if it is not already
-		
+
 		Parameters:
 			- workbookName (string): The name of the workbook to open
-		
+
 		Returns: None
 		'''
 
@@ -169,7 +169,7 @@ class PortDetails ( ):
 
 	'''
 	Class name: PortDetails
-	
+
 	Class Purpose: To obtain individual port details from Solarwinds
 	'''
 
@@ -177,14 +177,14 @@ class PortDetails ( ):
 
 		'''
 		Method name: __init__
-		
+
 		Method Purpose: To initialize a port detail instance
-		
-		Parameters: 
+
+		Parameters:
 			- ipAddress (string): The ip address the port is bound to
 			- username (string): The username of the user in solarwinds
 			- password (string): The password of the user in solarwinds
-		
+
 		Returns: None
 		'''
 
@@ -198,40 +198,40 @@ class PortDetails ( ):
 		self.__solarwinds = SwisClient ( "solarwinds.ameren.com" , username , password )
 
 	def getPortInfo ( self ):
-		
+
 		'''
 		Method name: getPortInfo
-		
+
 		Method Purpose: To get the important port information from Solarwinds
-		
+
 		Parameters: None
-		
+
 		Returns: A dictionary of the details of the query if it is found
 				 'None' if results are not found
 		'''
 
-		# portQueryResults = self.__solarwinds.query  ( 	"""
-		# 												SELECT
-		# 													p.PortDescription,
-		# 													p.PortType,
-		# 													p.PortID,
-		# 													p.Speed,
-		# 													p.Duplex
-		# 												FROM
-		# 													Orion.UDT.Port p
-		# 												WHERE
-		# 													p.Node.IPAddress='{}'
-		# 												""".format ( self.__ipAddress )
-		# 											)
-	
 		portQueryResults = self.__solarwinds.query  ( 	"""
 														SELECT
-															p.NodeID
+															p.PortDescription,
+															p.PortType,
+															p.PortID,
+															p.Speed,
+															p.Duplex
 														FROM
-															Orion.Nodes p 
+															Orion.UDT.Port p
 														WHERE
-															p.IPAddress='{}'
+															p.Node.IPAddress='{}'
 														""".format ( self.__ipAddress )
 													)
-	
+
+		# portQueryResults = self.__solarwinds.query  ( 	"""
+		# 												SELECT
+		# 													p.NodeID
+		# 												FROM
+		# 													Orion.Nodes p
+		# 												WHERE
+		# 													p.IPAddress='{}'
+		# 												""".format ( self.__ipAddress )
+		# 											)
+
 		return portQueryResults [ 'results' ]
